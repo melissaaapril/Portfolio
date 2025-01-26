d75e874a83496ec6a47e5cf638c953efd909d564
Melissa Castro <castromelissa2004@gmail.com>
2025-01-26 14:24:26 -0800

    updated dependencies and added img files and folders for components and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+//this page will contain the routing or the navigation for the app
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
